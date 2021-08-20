9d983b5502a35d3ba9139e8843bce171d537f20c
= <=>
2021-08-20 16:48:59 -0500

    randomized computer robot

--- assets/js/game.js
+++ assets/js/game.js
@@ -36,18 +36,34 @@ var fightOrSkip = function() {
   }
 // fight function
 var fight = function(enemy) {
+    // keep track of who goes first
+    var isPlayerTurn = true;
+    if (Math.random()> 0.5) {
+        isPlayerTurn = false;
+    }
+
     while (playerInfo.health > 0 && enemy.health > 0 ){
-        if (fightOrSkip()) {
-            // if true, leave fight by breaking loop
-            break;
-        } 
+        if (isPlayerTurn) {
+            // ask player if they'd like to fight or skip using fightOrSkip function
+            if (fightOrSkip()) {
+                //if true, leave fight by breaking loop
+                break;
+            } 
         
         // generate random damage value based on player's attack power
 var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
+//remove enemy's health by subtracting the amount we set in the damage variable
 enemy.health = Math.max(0, enemy.health - damage);
 console.log(
-    playerInfo.name + " attacked " + enemy.name + " . " + enemy.name + " now has " + enemy.health + " health remaining."
+    playerInfo.name +
+     " attacked " +
+      enemy.name +
+       " . " +
+        enemy.name +
+         " now has " +
+          enemy.health +
+           " health remaining."
     );
     
     // check enemy's health
@@ -62,11 +78,12 @@ console.log(
     } else {
         window.alert(enemy.name + " sill has " + enemy.health + " health left. ");
     }
+    //player gets attacked first
+} else  {
     
     var damage = randomNumber(enemy.attack - 3, enemy.attack);
     
-    playerInfo.health = Math.max(0, playerInfo.health - damage)
-    ;
+    playerInfo.health = Math.max(0, playerInfo.health - damage);
     console.log(
         enemy.name + " attacked " + playerInfo.name + " . " + playerInfo.name + " now has " + playerInfo.health + " health remaining."
         );
@@ -79,6 +96,9 @@ console.log(
         } else {
             window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
         }
+    }
+    // swith turn order for next round
+    isPlayerTurn = !isPlayerTurn;
     }
 };
         
